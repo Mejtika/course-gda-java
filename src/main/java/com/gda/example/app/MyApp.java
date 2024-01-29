ed0c84b0cc086fb73e021b32bb288982f9af11d5
mejtika <mejtika@hotmail.com>
2024-01-29 21:07:54 +0100

    feat: add code that super linter won't like

--- src/main/java/com/gda/example/app/MyApp.java
+++ src/main/java/com/gda/example/app/MyApp.java
@@ -4,4 +4,10 @@ public class MyApp {
     public static int sum(int a, int b) {
         return a + b;
     }
+
+    // This function will be reported as an error by the Super Linter
+    public 
+    static void       myFunction() {
+        // TODO: Implement the function
+    }
 }
