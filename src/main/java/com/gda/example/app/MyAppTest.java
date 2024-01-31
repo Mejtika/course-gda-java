bbc35c5c597c013c3abdf1be41dec4adb2e5cfb9
mejtika <mejtika@hotmail.com>
2024-01-31 19:27:12 +0100

    feat: add more stuff to test class

--- src/main/java/com/gda/example/app/MyAppTest.java
+++ src/main/java/com/gda/example/app/MyAppTest.java
@@ -1,5 +1,10 @@
 package com.gda.example.app;
 
 public class MyAppTest {
-    
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int result = MyApp.sum(a, b);
+        System.out.println("Result of sum(" + a + ", " + b + ") is " + result);
+    }
 }
