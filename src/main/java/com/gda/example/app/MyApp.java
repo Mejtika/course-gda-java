32601747e3e32d4ca87f98c2609e0f167dcc7d96
mejtika <mejtika@hotmail.com>
2024-01-31 19:11:31 +0100

    feat: clean myapp class

--- src/main/java/com/gda/example/app/MyApp.java
+++ src/main/java/com/gda/example/app/MyApp.java
@@ -1,14 +1,12 @@
 package com.gda.example.app;
 
 public class MyApp {
-    public static int sum(int a, 
-    int b) {
+    public static int sum(int a, int b) {
         return a + b;
     }
 
     // This function will be reported as an error by the Super Linter
-    public 
-    static void       myFunction() {
+    public static void myFunction() {
         // TODO: Implement the function
     }
 }
